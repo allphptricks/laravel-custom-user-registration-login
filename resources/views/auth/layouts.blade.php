b64f77d0578f997fcb37cdb6870d55df5d540eef
AllPHPTricks <124980595+allphptricks@users.noreply.github.com>
2023-10-23 15:34:43 +0500

    Update layouts.blade.php
    Updated back to tutorial link in layouts.blade.php

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 9 Custom User Registration & Login Tutorial - AllPHPTricks.com</title>
+    <title>Laravel 10 Custom User Registration & Login Tutorial - AllPHPTricks.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
@@ -47,7 +47,18 @@
     </nav>    
 
     <div class="container">
+        <h3 class="text-center mt-3 mb-3">Laravel 10 Custom User Registration & Login</h3>
         @yield('content')
+        <div class="row justify-content-center text-center mt-3">
+            <div class="col-md-12">
+                <p>Back to Tutorial: 
+                    <a href="https://www.allphptricks.com/laravel-custom-user-registration-and-login-tutorial/"><strong>Tutorial Link</strong></a>
+                </p>
+                <p>
+                    For More Web Development Tutorials Visit: <a href="https://www.allphptricks.com/"><strong>AllPHPTricks.com</strong></a>
+                </p>
+            </div>
+        </div>
     </div>
        
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>    
